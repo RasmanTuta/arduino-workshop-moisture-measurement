808791f3f5ea3dbfa514e2a7b9ada6079b43e4ae
Kristian Berg <rasmantuta@gmail.com>
2018-04-23 22:49:59 +0200

    Added skelton for final solution

---
+++ arduino-workshop-moisture-measurement.ino
@@ -0,0 +1,24 @@
+// Add necessary includes
+
+// set up drivers and global variables
+
+void setup() {
+  // Initialize stuff
+}
+
+void loop(){
+  // Do measurements and display stuff
+}
+
+void readAir(){
+  
+}
+
+void readSoil(){
+  
+}
+
+void displayMeasurements(int airTemperature, int airHumidity, int soilHumidity, bool humidityAlarm){
+  
+}
+
